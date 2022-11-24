adefcc64594cea247e4a89fb51df337220db7e2a
jefferson <berronesjefferson@gmail.com>
2022-11-23 19:37:36 -0500

    p01-feat: move function to specific handle

--- static/v2/js/common/app-system/index.js
+++ static/v2/js/common/app-system/index.js
@@ -60,51 +60,68 @@ const AppSystem = {
             getConstructId: function (id, suffix) {
                 return `${AppSystem.handleComp.CONSTANT.prefix}${id}${suffix}`
             },
-            onlyShow: function ({id = ''}) {
-                const $alert = document.getElementById(this.getConstructId(id, this.CONSTANT.alertSuffix))
-                $alert.classList.remove('d-none')
-            },
-            show: function (data = {}) {
-                const {id = '', title = '', content = '', typeFeature = AppSystem.CONSTANT.typeFeature.info, withTitleDefault = true} = data
+            alert: {
+                onlyShow: function ({id = ''}) {
+                    const $alert = document.getElementById(AppSystem.handleComp.alert.getConstructId(id, AppSystem.handleComp.alert.CONSTANT.alertSuffix))
+                    $alert.classList.remove('d-none')
+                },
+                show: function (data = {}) {
+                    const {id = '', title = '', content = '', typeFeature = AppSystem.CONSTANT.typeFeature.info, withTitleDefault = true} = data
 
-                const $alert = document.getElementById(this.getConstructId(id, this.CONSTANT.alertSuffix))
-                const $title = document.getElementById(this.getConstructId(id, this.CONSTANT.titleSuffix))
-                const $content = document.getElementById(this.getConstructId(id, this.CONSTANT.contentSuffix))
+                    const $alert = document.getElementById(AppSystem.handleComp.alert.getConstructId(id, AppSystem.handleComp.alert.CONSTANT.alertSuffix))
+                    const $title = document.getElementById(AppSystem.handleComp.alert.getConstructId(id, AppSystem.handleComp.alert.CONSTANT.titleSuffix))
+                    const $content = document.getElementById(AppSystem.handleComp.alert.getConstructId(id, AppSystem.handleComp.alert.CONSTANT.contentSuffix))
 
-                let titleDefault = ''
+                    let titleDefault = ''
 
-                if (typeFeature === AppSystem.CONSTANT.typeFeature.primary) {
-                    titleDefault = 'Informacion'
-                } else if (typeFeature === AppSystem.CONSTANT.typeFeature.secondary) {
-                    titleDefault = 'Informacion'
-                } else if (typeFeature === AppSystem.CONSTANT.typeFeature.success) {
-                    titleDefault = 'Exitoso'
-                } else if (typeFeature === AppSystem.CONSTANT.typeFeature.danger) {
-                    titleDefault = 'Error'
-                } else if (typeFeature === AppSystem.CONSTANT.typeFeature.warning) {
-                    titleDefault = 'Alerta'
-                } else if (typeFeature === AppSystem.CONSTANT.typeFeature.info) {
-                    titleDefault = 'Informacion'
-                } else if (typeFeature === AppSystem.CONSTANT.typeFeature.light) {
-                    titleDefault = 'Informacion'
-                } else if (typeFeature === AppSystem.CONSTANT.typeFeature.dark) {
-                    titleDefault = 'Informacion'
+                    if (typeFeature === AppSystem.CONSTANT.typeFeature.primary) {
+                        titleDefault = 'Informacion'
+                    } else if (typeFeature === AppSystem.CONSTANT.typeFeature.secondary) {
+                        titleDefault = 'Informacion'
+                    } else if (typeFeature === AppSystem.CONSTANT.typeFeature.success) {
+                        titleDefault = 'Exitoso'
+                    } else if (typeFeature === AppSystem.CONSTANT.typeFeature.danger) {
+                        titleDefault = 'Error'
+                    } else if (typeFeature === AppSystem.CONSTANT.typeFeature.warning) {
+                        titleDefault = 'Alerta'
+                    } else if (typeFeature === AppSystem.CONSTANT.typeFeature.info) {
+                        titleDefault = 'Informacion'
+                    } else if (typeFeature === AppSystem.CONSTANT.typeFeature.light) {
+                        titleDefault = 'Informacion'
+                    } else if (typeFeature === AppSystem.CONSTANT.typeFeature.dark) {
+                        titleDefault = 'Informacion'
+                    }
+
+                    if (title.length) {
+                        $title.innerHTML = title
+                    } else {
+                        if (withTitleDefault)
+                            $title.innerHTML = titleDefault
+                    }
+
+                    $content.innerHTML = content
+                    $alert.classList.remove('d-none')
+                },
+                hidden: function ({id = ''}) {
+                    const $alert = document.getElementById(AppSystem.handleComp.alert.getConstructId(id, AppSystem.handleComp.alert.CONSTANT.alertSuffix))
+                    $alert.classList.add('d-none')
                 }
-
-                if (title.length) {
-                    $title.innerHTML = title
-                } else {
-                    if (withTitleDefault)
-                        $title.innerHTML = titleDefault
-                }
-
-                $content.innerHTML = content
-                $alert.classList.remove('d-none')
-            },
-            hidden: function ({id = ''}) {
-                const $alert = document.getElementById(this.getConstructId(id, this.CONSTANT.alertSuffix))
-                $alert.classList.add('d-none')
             }
-        }
+        },
+        card: {
+            CONSTANT: {
+                alertSuffix: 'Alert',
+                titleSuffix: 'titleAlert',
+                contentSuffix: 'contentAlert'
+            },
+            getConstructId: function (id, suffix) {
+                return `${AppSystem.handleComp.CONSTANT.prefix}${id}${suffix}`
+            },
+            cardWidgetValue: {
+                changeValue: function ({id = ''}) {
+                    
+                }
+            },
+        },
     }
 }
