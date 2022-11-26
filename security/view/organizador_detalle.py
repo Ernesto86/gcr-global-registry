705a16c5a521148c1f5d6519b87aa48d12b4f711
jefferson <berronesjefferson@gmail.com>
2022-11-26 17:40:54 -0500

    p01-24: feat organizador de registro

--- security/view/organizador_detalle.py
+++ security/view/organizador_detalle.py
@@ -1,11 +1,14 @@
 from urllib.parse import urlencode
-from django.db.models import Q
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.views.generic import ListView
+
 from core.util_functions import ListViewFilter
 from institutions.models import InsTypeRegistries
-from students.models import StudentRegisters
 from security.functions import addUserData
+from students.models import StudentRegisters
+
 
 class OrganizadorRegistroListView(ListViewFilter, LoginRequiredMixin, ListView):
     login_url = '/security/login'
@@ -17,7 +20,8 @@ class OrganizadorRegistroListView(ListViewFilter, LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         addUserData(self.request, context)
-        context['nameregister'] = InsTypeRegistries.objects.get(id=self.kwargs.get("typeregisterid"))
+        ins_type_registries = InsTypeRegistries.objects.get(id=self.kwargs.get("typeregisterid"))
+        context['title_label'] = f'Registros de {ins_type_registries.name} - {ins_type_registries.detail}'
         get_params = {k: v[0] for k, v in dict(self.request.GET).items()}
         context.update(get_params)
         try:
@@ -27,8 +31,7 @@ class OrganizadorRegistroListView(ListViewFilter, LoginRequiredMixin, ListView):
         context['url_params'] = urlencode(get_params)
         return context
 
-
-    def get_queryset(self,**kwargs):
+    def get_queryset(self, **kwargs):
         search = self.request.GET.get('search', '')
 
         if search:
@@ -42,14 +45,14 @@ class OrganizadorRegistroListView(ListViewFilter, LoginRequiredMixin, ListView):
         else:
             typeregister = self.kwargs.get("typeregisterid")
             return StudentRegisters.objects.select_related(
-                    "institution",
-                    "student",
-                    "type_register",
-                    "certificate",
-                    "country"
-                ).filter(
-                     type_register_id=typeregister,
-                     institution__created_by=self.request.user.username
-                ).order_by(
-                    "-date_issue"
-                )
+                "institution",
+                "student",
+                "type_register",
+                "certificate",
+                "country"
+            ).filter(
+                type_register_id=typeregister,
+                institution__created_by=self.request.user.username
+            ).order_by(
+                "-date_issue"
+            )
