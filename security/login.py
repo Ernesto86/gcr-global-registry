34fd636930dc26f648aa7cc7026d17f4c104f828
jefferson <berronesjefferson@gmail.com>
2022-11-28 23:28:35 -0500

    p01-24: feat login and recatpcha

--- security/login.py
+++ security/login.py
@@ -1,11 +1,18 @@
+import os
+import requests
+import json
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
 from django.http import JsonResponse
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
+
+from core.common.filter_query.filter_query_common import FilterQueryCommon
 from security.functions import addUserData
 from django.views.generic import RedirectView
+
 
 class LoginAuthView(LoginView):
     form_class = AuthenticationForm
@@ -36,16 +43,44 @@ class LoginAuthView(LoginView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Inicio de Sesión'
         addUserData(self.request, context)
+        context['recaptcha_site_key'] = os.environ.get('RECAPTCHA_SITE_KEY', '')
         return context
 
     def post(self, request, *args, **kwargs):
         data = {'errors': []}
         status = 200
-        try:
-            norobot = request.POST.get('norobot', '')
-            if not norobot:
-                raise Exception("Login Fallido!, acceso No autorizado.")
 
+        recaptcha = FilterQueryCommon.get_param_validate(self.request.POST.get("g-recaptcha-response", None))
+        recaptcha_secret_key = os.environ.get('RECAPTCHA_SECRET_KEY', '')
+
+        recaptcha_data = {
+            "secret": recaptcha_secret_key,
+            "response": recaptcha
+        }
+
+        try:
+            response = requests.post(
+                'https://www.google.com/recaptcha/api/siteverify?secret={}&response={}'.format(
+                    recaptcha_secret_key,
+                    recaptcha
+                ),
+                data=json.dumps(recaptcha_data)
+            )
+        except Exception as e:
+            data['errors'] = ["Error de captcha"]
+            return JsonResponse(data, status=400)
+
+        if response.status_code != 200:
+            data['errors'] = ["Error de captcha"]
+            return JsonResponse(data, status=400)
+
+        response_json = response.json()
+
+        if not response_json['success']:
+            data['errors'] = ["Error de captcha"]
+            return JsonResponse(data, status=400)
+
+        try:
             cuenta = str(request.POST.get('username')).strip()
             password = str(request.POST.get('password')).strip()
 
@@ -56,23 +91,21 @@ class LoginAuthView(LoginView):
                     data['code'] = 'success'
                     data['message'] = 'Login user successful'
                 else:
-                    data['code'] = 'failed'
                     status = 400
                     data['message'] = 'Login user session failed'
                     data['errors'].append('Login Fallido!, usuario no esta habilitado')
             else:
-                data['code'] = 'failed'
                 status = 400
                 data['message'] = 'Login user session failed'
                 data['errors'].append('Login Fallido!, credenciales incorrectas.')
 
         except Exception as e:
             status = 500
-            data['code'] = 'failed'
             data['message'] = 'Internal error in code'
             data['errors'].append(str(e))
 
         return JsonResponse(data, status=status)
+
 
 class LogoutRedirectView(RedirectView):
     pattern_name = 'login'
